5f9a01f28f953007260b68866107a35f3143f930
whoisatharv <atharvavyas72@gmail.com>
2025-02-02 11:17:47 +0530

    add today work

---
+++ day40addsumLinkedlist.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode();
+        ListNode* temp = dummy;
+        int carry = 0;
+        
+        while (l1 != nullptr || l2 != nullptr || carry) {
+            int sum = 0;
+            
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            
+            sum += carry;
+            carry = sum / 10;
+            
+            ListNode* node = new ListNode(sum % 10);
+            temp->next = node;
+            temp = temp->next;
+        }
+        
+        return dummy->next;
+    }
+};
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        std::cout << head->val << " ";
+        head = head->next;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    // Example usage
+    ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+    
+    Solution solution;
+    ListNode* result = solution.addTwoNumbers(l1, l2);
+    
+    printList(result);
+    
+    return 0;
+}
